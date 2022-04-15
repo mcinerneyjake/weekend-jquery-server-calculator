31b54ec20165ec36fb0f10ebda3a921593680e5f
Jake McInerney <jakemcinerney1@gmail.com>
2022-04-14 19:48:37 -0500

    server is up and running

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static('./server/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = 5000;
+
+app.listen(PORT, function () {
+  console.log(`The server is running! Check it out at http://localhost:${PORT}!`);
+});
