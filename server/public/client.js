b2c66c2e1462d9130df0fada5f5d22e2e74de27a
Jake McInerney <jakemcinerney1@gmail.com>
2022-04-14 20:12:29 -0500

    created math equations in client, waiting to be transferred server-side

--- server/public/client.js
+++ server/public/client.js
@@ -3,3 +3,26 @@ $(document).ready(onReady);
 function onReady() {
   console.log('jQuery');
 }
+
+/////////////////////////////////////////////////////////
+// FUCTIONS TO BE PUT ONTO THE SERVER
+function addNumbers(number1, number2) {
+  let addedNumbers = number1 + number2;
+  $('#equationContainer').append(addedNumbers);
+}
+
+function subtractNumbers(number1, number2) {
+  let subtractedNumbers = number1 - number2;
+  $('#equationContainer').append(subtractedNumbers);
+}
+
+function multiplyNumbers(number1, number2) {
+  let multipliedNumbers = number1 * number2;
+  $('#equationContainer').append(multipliedNumbers);
+}
+
+function divideNumbers(number1, number2) {
+  let dividedNumbers = number1 / number2;
+  $('#equationContainer').append(dividedNumbers);
+}
+/////////////////////////////////////////////////////////
