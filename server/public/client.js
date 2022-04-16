3957a50a3734666407d6e5694ebf11de81079574
Jake McInerney <jakemcinerney1@gmail.com>
2022-04-15 21:41:01 -0500

    added initial POST and GET ajax methods in client

--- server/public/client.js
+++ server/public/client.js
@@ -4,15 +4,31 @@ function onReady() {
   console.log('jQuery');
   $('#equalsButton').on('click', mathEquationOnSubmit);
   $('#clearButton').on('click', clearCalculator);
+  $('#addButton').on('click', handleMathOperatorValues);
+  $('#subtractButton').on('click', handleMathOperatorValues);
+  $('#multiplyButton').on('click', handleMathOperatorValues);
+  $('#divideButton').on('click', handleMathOperatorValues);
+}
+
+let valueOfButtonOperator = '';
+
+function handleMathOperatorValues() {
+  valueOfButtonOperator = $(this).text();
 }
 
 function mathEquationOnSubmit() {
-  let newMathEquation = {
+  let storeMathEquationObject = {
     firstNumber: Number($('#firstNumberInput').val()),
     secondNumber: Number($('#secondNumberInput').val()),
+    mathOperator: valueOfButtonOperator,
   };
-  console.log(newMathEquation);
+  sendMathDataToServer(storeMathEquationObject);
   clearCalculator();
+}
+
+function clearCalculator() {
+  $('#firstNumberInput').val('');
+  $('#secondNumberInput').val('');
 }
 
 /////////////////////////////////////////////////////////
@@ -37,10 +53,30 @@ function divideNumbers(number1, number2) {
   $('#equationContainer').append(dividedNumbers);
 }
 
-function clearCalculator() {
-  $('#firstNumberInput').val('');
-  $('#secondNumberInput').val('');
+/////////////////////////////////////////////////////////
+
+function sendMathDataToServer(data) {
+  $.ajax({
+    method: 'POST',
+    url: '/calculator',
+    data: data,
+  }).then(function (response) {
+    console.log('response of POST is:');
+    console.log(response);
+    getMathDataFromServer();
+  });
 }
 
-function collectMathData() {}
-/////////////////////////////////////////////////////////
+function getMathDataFromServer() {
+  $.ajax({
+    method: 'GET',
+    url: '/calculator',
+  }).then(function (response) {
+    console.log('the server sent back the completed equation:');
+    console.log(response);
+    $('#equationContainer').empty();
+    for (let equation of response) {
+      $('#equationContainer').append(`<li>${equation.firstNumber} ${equation.mathOperator} ${equation.secondNumber}`);
+    }
+  });
+}
